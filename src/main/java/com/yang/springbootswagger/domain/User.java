aeff232f1c284a9c9017a7a92142732ab7fc9c40
JaxYoun <gao88jie@qq.com>
2018-04-25 23:34:43 +0800

    补充部Swagger分注解

--- src/main/java/com/yang/springbootswagger/domain/User.java
+++ src/main/java/com/yang/springbootswagger/domain/User.java
@@ -1,11 +1,17 @@
 package com.yang.springbootswagger.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 
+@ApiModel(value = "User", description = "用户实体类")
 public class User implements Serializable {
 
+    @ApiModelProperty(value = "学号")
     private Long id;
 
+    @ApiModelProperty(value = "姓名")
     private String name;
 
     public Long getId() {
--- src/main/java/com/yang/springbootswagger/rest/UserController.java
+++ src/main/java/com/yang/springbootswagger/rest/UserController.java
@@ -1,20 +1,28 @@
 package com.yang.springbootswagger.rest;
 
 import com.yang.springbootswagger.domain.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
 
-@Api("用户业务相关接口")
+@Api
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    /**
+     * @param id
+     * @param name
+     * @return
+     */
     @PostMapping("/getUser")
     @ApiOperation(value = "根据id查询用户信息", notes = "查询数据库中某个的用户信息")
-//    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
-//    @ApiImplicitParam(name = "name", value = "用户姓名", required = true, dataType = "String")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "name", value = "用户姓名", required = true, dataType = "String")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "成功！", response = User.class)
+    })
     public User getUser(Long id, String name) {
         User user1 = new User();
         user1.setId(id);
@@ -22,9 +30,13 @@ public class UserController {
         return user1;
     }
 
+    /**
+     * @param user
+     * @return
+     */
     @PostMapping("/getUserBy")
     @ApiOperation(value = "条件查询用户", notes = "根据条件查询用户信息")
-    public User getUserBy(@RequestBody User user) {
+    public User getUserBy(@RequestBody @ApiParam(value = "用户", name = "user", required = true) User user) {
         User user1 = new User();
         user1.setId(user.getId());
         user1.setName(user.getName());
